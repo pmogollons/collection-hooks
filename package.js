cbe31902e76e19d26c5ebaf72e19dc9512bfe7dd
Paulo Mogollón <pmogollon@unbosque.edu.co>
2024-09-03 10:17:25 -0500

    Published version 1.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "pmogollons:collection-hooks",
-  version: "1.0.2",
+  version: "1.0.3",
   summary: "Collection hooks for Meteor",
   git: "https://github.com/pmogollons/collection-hooks",
   documentation: "README.md",
