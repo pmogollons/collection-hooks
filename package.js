72b74180ef1e8ed115b26c870201d40daa678bb1
Paulo Mogollon <pmogollon@propiedata.com>
2024-11-04 12:01:11 -0500

    Published version 1.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "pmogollons:collection-hooks",
-  version: "1.0.4",
+  version: "1.0.5",
   summary: "Collection hooks for Meteor",
   git: "https://github.com/pmogollons/collection-hooks",
   documentation: "README.md",
