b6e8bc9b00727f1514329d53f8e3bf59ad30c12e
Paulo Mogollón <pmogollon@propiedata.com>
2025-08-29 15:08:37 -0500

    Improvements to types

--- collection-hooks.d.ts
+++ collection-hooks.d.ts
@@ -1,46 +1,34 @@
-/* eslint no-unused-vars: 0 */
+type HookRemoveFunction = () => void;
+
+type Deps = {
+  [field: string]: boolean | 1 | -1 | Deps
+}
 
 declare module "meteor/pmogollons:collection-hooks" {
   interface CollectionHooks {
-    onError: (callback: (error: Error) => void) => void;
+    onError(callback: (error: Error) => void): HookRemoveFunction;
   }
 }
 
 declare module "meteor/mongo" {
-  type AnyObject = Record<string, any>;
-
-  type DependencyGraph = {
-    [field: string]: DependencyGraph
-  }
-
-  type FilterParams = {
-    filters: AnyObject;
-    options: AnyObject;
-    params: AnyObject;
-  }
-
-  type Body<T> = {
-    [field: string]: DependencyGraph | Body<T>;
-  }
-
-  type HookParams = {
-    doc: AnyObject;
-    previousDoc?: Body<any>;
+  type HookParams<U> = {
+    doc: Partial<U>;
+    previousDoc?: Partial<U>;
     userId?: string;
   }
 
   type HookOptions = {
-    docFields?: AnyObject;
+    docFields?: Deps;
     fetchPrevious?: boolean;
   }
 
   namespace Mongo {
-    interface Collection<T, U = T> {
-      onInsert(callback: (params: HookParams) => void, options?: HookOptions): void;
-      onUpdate(callback: (params: HookParams) => void, options?: HookOptions): void;
-      onRemove(callback: (params: HookParams) => void, options?: HookOptions): void;
-      onBeforeInsert(callback: (params: HookParams) => void): void;
-      onBeforeUpdate(callback: (params: HookParams) => void): void;
+    interface Collection<T = any, U = T> {
+      onInsert(callback: (params: HookParams<U>) => void, options?: HookOptions): HookRemoveFunction;
+      onUpdate(callback: (params: HookParams<U>) => void, options?: HookOptions): HookRemoveFunction;
+      onRemove(callback: (params: HookParams<U>) => void, options?: HookOptions): HookRemoveFunction;
+      onBeforeInsert(callback: (params: HookParams<U>) => void): HookRemoveFunction;
+      onBeforeUpdate(callback: (params: HookParams<U>) => void): HookRemoveFunction;
     }
   }
 }
